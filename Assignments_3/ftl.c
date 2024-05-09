f821c6e322ff3199f096bf95ca1b5a9e467fc3a4
박성진 <106302098+macboy5@users.noreply.github.com>
2024-05-09 12:55:19 +0900

    Create Assignemtns_3

---
+++ Assignments_3/ftl.c
@@ -0,0 +1,179 @@
+// 주의사항
+// 1. blockmapping.h에 정의되어 있는 상수 변수를 우선적으로 사용해야 함
+// 2. blockmapping.h에 정의되어 있지 않을 경우 본인이 이 파일에서 만들어서 사용하면 됨
+// 3. 필요한 data structure가 필요하면 이 파일에서 정의해서 쓰기 바람(blockmapping.h에 추가하면 안됨)
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include "blockmapping.h"
+// 필요한 경우 헤더 파일을 추가하시오.
+
+//
+// flash memory를 처음 사용할 때 필요한 초기화 작업, 예를 들면 address mapping table에 대한
+// 초기화 등의 작업을 수행한다. 따라서, 첫 번째 ftl_write() 또는 ftl_read()가 호출되기 전에
+// file system에 의해 반드시 먼저 호출이 되어야 한다.
+//
+
+int dd_read(int ppn, char *pagebuf);
+int dd_write(int ppn, char *pagebuf);
+int dd_erase(int pbn);
+
+int address_mapping_table[DATABLKS_PER_DEVICE];
+int free_block = 0;
+int block_indx = 1;
+
+void ftl_open()
+{
+	//
+	// address mapping table 초기화 또는 복구
+	// free block's pbn 초기화
+    // address mapping table에서 lbn 수는 DATABLKS_PER_DEVICE 동일
+
+    //address mapping table 값을 전부 -1 로 초기화
+    memset(address_mapping_table, -1, sizeof(address_mapping_table));
+
+    //초기 free block pbn = 0.
+    free_block=0;
+
+	return;
+}
+
+
+//
+// 이 함수를 호출하는 쪽(file system)에서 이미 sectorbuf가 가리키는 곳에 512B의 메모리가 할당되어 있어야 함
+// (즉, 이 함수에서 메모리를 할당 받으면 안됨)
+//
+void ftl_read(int lsn, char *sectorbuf)
+{
+    int lbn = lsn / PAGES_PER_BLOCK;
+    int pbn = address_mapping_table[lbn];
+    int offset = lsn % PAGES_PER_BLOCK;
+
+    char pagebuf[PAGE_SIZE];
+    if( dd_read(pbn * PAGES_PER_BLOCK + offset, pagebuf) == 1 ){
+//        printf("%s\n", pagebuf);
+        memcpy(sectorbuf, pagebuf, SECTOR_SIZE);
+ //       printf("READ() SECTOR DATA : %s\n", sectorbuf);
+
+        char sparebuf[SPARE_SIZE];
+        memcpy(sparebuf, pagebuf+SECTOR_SIZE, SPARE_SIZE);
+        char first_four_bytes[SPARE_SIZE/4];
+        memcpy(first_four_bytes, sparebuf, SPARE_SIZE/4);
+ //     printf("READ() SPARE DATA : %s\n", first_four_bytes);
+    }
+    else{
+        fprintf(stderr, "flash memory read error\n");
+    }
+
+	return;
+}
+
+
+//
+// 이 함수를 호출하는 쪽(file system)에서 이미 sectorbuf가 가리키는 곳에 512B의 메모리가 할당되어 있어야 함
+// (즉, 이 함수에서 메모리를 할당 받으면 안됨)
+//
+void ftl_write(int lsn, char *sectorbuf)
+{
+    int lbn = lsn / PAGES_PER_BLOCK;
+    int pbn = address_mapping_table[lbn];
+    int offset = lsn % PAGES_PER_BLOCK;
+
+    char pagebuf[PAGE_SIZE];
+
+    //pagebuf에 data 적는 과정..
+    //pagebuf의 sector area에  sectorbuf 내용 쓰기
+    memcpy(pagebuf, sectorbuf, SECTOR_SIZE);
+//    printf("WRITE() SECTOR DATA : %s\n", pagebuf);
+
+    char lsnbuf[SPARE_SIZE/4];
+    sprintf(lsnbuf,  "%d", lsn);
+//    printf("WRITE() SPARE DATA : %s\n", lsnbuf);
+
+    //pagebuf의 spare area에  lsn 쓰기
+    memcpy(pagebuf+SECTOR_SIZE, lsnbuf, SPARE_SIZE/4);
+
+    //printf("PAGE DATA : ")
+
+    //lsn에 최초로 데이터를 쓰는 경우
+    if( pbn == -1 ){
+
+        //block_indx번째 블록으로 이동 후 write.
+        dd_write(block_indx * PAGES_PER_BLOCK + offset, pagebuf);
+
+        //address mapping table update.
+        address_mapping_table[lbn] = block_indx++;
+
+    }
+    // 해당 블럭에 -1이 아닌 값이 적혀있는 경우..
+    // pbn의 offset이 비어있어서 가서 바로 적을 수 있는 경우와 free block으로 copy 떠줘야하는 경우로 나뉨..
+    //spare area에 값이 적혀있는지 아닌지로 나누어줌..
+    else{
+        char pagebuf_tmp[PAGE_SIZE];
+
+        char all_ff[SPARE_SIZE/4];
+        memset(all_ff, 0xff, SPARE_SIZE/4);
+
+
+        dd_read(pbn*PAGES_PER_BLOCK+offset, pagebuf_tmp);
+
+//        printf("%d\n", *spare_area);
+
+        char first_four_bytes[SPARE_SIZE/4];
+        memcpy(first_four_bytes, pagebuf_tmp+SECTOR_SIZE, SPARE_SIZE/4);
+
+
+        //offset 위치가 비어있는 경우에는 그냥 쓰면 되고
+        if(memcmp(first_four_bytes, all_ff , SPARE_SIZE/4) == 0 ){
+
+            dd_write(pbn*PAGES_PER_BLOCK+offset, pagebuf);
+
+        }
+        //offset 위치에 이미 적혀있는 경우에는 free block에 옮겨주고 offset에 적는다.
+        else{
+            //free block에 copy.
+            for(int i=0; i<4; i++) {
+                if (i != offset) {
+                    char lsn_area_tmp[SPARE_SIZE/4];
+                    dd_read(pbn * PAGES_PER_BLOCK + i, pagebuf_tmp);
+                    memcpy(lsn_area_tmp, pagebuf_tmp+SECTOR_SIZE, SPARE_SIZE/4);
+                    if(memcmp(lsn_area_tmp, all_ff , SPARE_SIZE) != 0 ) {
+                        //printf("%d\n", *spare_area);
+                        dd_write(free_block * PAGES_PER_BLOCK + i, pagebuf_tmp);
+                    }
+                }
+            }
+
+            dd_write(free_block*PAGES_PER_BLOCK + offset, pagebuf);
+
+            //기존 pbn erase.
+            dd_erase(pbn);
+
+            //free_block 변경 및 address mapping table update.
+            address_mapping_table[lbn] = free_block;
+            free_block = pbn;
+
+        }
+
+    }
+
+    return;
+}
+
+void ftl_print()
+{
+    printf("lbn pbn\n");
+    for(int i = 0; i < DATABLKS_PER_DEVICE; i++) {
+        printf("%d %d\n",i, address_mapping_table[i]);
+    }
+
+    //free block number 출력...
+    printf("free block=%d\n",free_block);
+
+    //block_indx test
+    printf("block_indx=%d\n", block_indx);
+
+	return;
+}
---
+++ Assignments_3/main.c
@@ -0,0 +1,116 @@
+// 
+// 과제3의 채점 프로그램은 기본적으로 아래와 같이 동작함
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "blockmapping.h"
+
+FILE *flashmemoryfp;
+
+
+/****************  prototypes ****************/
+void ftl_open();
+void ftl_write(int lsn, char *sectorbuf);
+void ftl_read(int lsn, char *sectorbuf);
+void ftl_print();
+
+//
+// 이 함수는 file system의 역할을 수행한다고 생각하면 되고,
+// file system이 flash memory로부터 512B씩 데이터를 저장하거나 데이터를 읽어 오기 위해서는
+// 각자 구현한 FTL의 ftl_write()와 ftl_read()를 호출하면 됨
+//
+int main(int argc, char *argv[])
+{
+	char *blockbuf;
+    char sectorbuf[SECTOR_SIZE];
+	int lsn, i;
+
+
+    flashmemoryfp = fopen("flashmemory", "w+b");
+	if(flashmemoryfp == NULL)
+	{
+		printf("file open error\n");
+		exit(1);
+	}
+	   
+    //
+    // flash memory의 모든 바이트를 '0xff'로 초기화한다.
+    // 
+    blockbuf = (char *)malloc(BLOCK_SIZE);
+	memset(blockbuf, 0xFF, BLOCK_SIZE);
+
+	for(i = 0; i < BLOCKS_PER_DEVICE; i++)
+	{
+		fwrite(blockbuf, BLOCK_SIZE, 1, flashmemoryfp);
+	}
+
+	free(blockbuf);
+
+	ftl_open();    // ftl_read(), ftl_write() 호출하기 전에 이 함수를 반드시 호출해야 함
+
+	//
+	// ftl_write() 및 ftl_read() 테스트 코드 작성
+	//
+
+    ftl_write(10, "FIRST WRITE()");
+    ftl_read(10, sectorbuf);
+    ftl_write(10, "SECOND TRY");
+//    ftl_read(10, sectorbuf);
+//    ftl_read(10, sectorbuf);
+//    ftl_write(2, "Test 1 ");
+//    ftl_read(10, sectorbuf);
+//    ftl_write(3, "Dfdfa");
+//    ftl_write(0, "Dfafas");
+//    ftl_write(3, "Dfdfa");
+//    ftl_write(20, "Test 54541");
+//
+//    ftl_read(20, sectorbuf);
+//    ftl_write(13, "Test 1");
+//    ftl_write(14, "Test 1");
+//    ftl_write(16, "Test 1");
+//    ftl_write(20, "CALL ME BABYYYYY");
+//    ftl_read(20, sectorbuf);
+//    ftl_read(20, sectorbuf);
+//    ftl_write(20, "TEST 3");
+//    ftl_read(20, sectorbuf);
+//    ftl_write(30,"TEST 2 ");
+//    ftl_read(30, sectorbuf);
+//    ftl_write(50, "TETE");
+//    ftl_read(50, sectorbuf);
+//    ftl_write(50, "TETE");
+//    ftl_write(59, "dfd");
+//    ftl_read(59, sectorbuf);
+//    ftl_write(59, "dfd");
+//    ftl_write(3, "dfd");
+//    ftl_write(3, "dfd");
+//    ftl_write(5, "dfafasdf");
+//    ftl_write(5, "dfafasdf");
+//    ftl_write(5, "dfafasdf");
+//    ftl_write(9, "dfafasdf");
+//    ftl_write(9, "dfafasdf");
+//    ftl_write(12, "dfafasdf");
+//    ftl_write(16, "dfafasdf");
+//    ftl_write(20, "TEST 1");
+//    ftl_write(24, "TEST 1");
+//    ftl_write(32, "TEST 1");
+//    ftl_write(36, "TEST 1");
+//    ftl_write(40, "TEST 1");
+//    ftl_write(44, "TEST 1");
+//    ftl_write(52, "TEST 1");
+//
+//    ftl_write(0, "TEST 1");
+//    ftl_write(0, "ABCDEFGHIJK");
+
+
+ //   ftl_read(5, sectorbuf);
+
+   ftl_print();
+
+
+	fclose(flashmemoryfp);
+
+	return 0;
+}
